2b622e55d9997d4761ede78bd0c2c7c7f1a6316d
Elyashiv <Elyashiv2468@gmail.com>
2021-11-23 18:26:16 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public void start(Stage stage) throws Exception{
+        Parent root = (Parent) FXMLLoader.load(getClass().getResource("Q2SB.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Q2SB");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println();
+    }
+}
