ab1bf5b4c7f3d64a1e4eca045ae5773f7e0b9f8f
Elyashiv <Elyashiv2468@gmail.com>
2021-12-07 17:42:47 +0200

    Made screen size constant

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main extends Application{
         Scene scene = new Scene(root);
         stage.setTitle("Q2SB");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
